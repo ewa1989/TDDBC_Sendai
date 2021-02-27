be65b869ac3cc56e824f917ca774a1692df09798
nakasho <19910245+nakasho-dev@users.noreply.github.com>
2021-02-27 12:18:08 +0900

    挨拶を実装

--- TDDBCSwift/Sample.swift
+++ TDDBCSwift/Sample.swift
@@ -1,5 +1,15 @@
+enum Greeting {
+    case greet
+    case please
+}
+
 class Sample {
-    func say() -> String {
-        return "Hello TDD BootCamp!"
+    func say(behavior: Greeting = .please) -> String {
+        switch behavior {
+        case .greet:
+            return "よろしくお願いします"
+        default:
+            return "Hello TDD BootCamp!"
+        }
     }
 }
