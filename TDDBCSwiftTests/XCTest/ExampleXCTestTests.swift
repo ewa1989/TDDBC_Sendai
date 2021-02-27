173b38404ab7eff156f7ae05cf37c660cdc83da2
UCHIDA Yusuke <>
2021-02-27 12:11:28 +0900

    挨拶する挙動を実装する前のテストケースを作成

--- TDDBCSwiftTests/XCTest/ExampleXCTestTests.swift
+++ TDDBCSwiftTests/XCTest/ExampleXCTestTests.swift
@@ -11,4 +11,13 @@ class ExampleXCTestTests: XCTestCase {
         let expected = "Hello TDD BootCamp!"
         XCTAssertEqual(actual, expected, diff(between: expected, and: actual))
     }
+    
+    func testGreeting() {
+        let sample = Sample()
+        
+        let actual = sample.say(behavior: .greet)
+        
+        let expected = "よろしくお願いします"
+        XCTAssertEqual(actual, expected, diff(between: expected, and: actual))
+    }
 }
