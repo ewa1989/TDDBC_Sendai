35383fc85999c42cd24ff82b06c0782f352fbee0
UCHIDA Yusuke <>
2021-03-06 10:44:21 +0900

    最初のテストファイルとTODOを作成

---
+++ TDDBCSwiftTests/XCTest/Strawberry/Model/StrawberryTest.swift
@@ -0,0 +1,39 @@
+//
+//  StrawberryTest.swift
+//  TDDBCSwiftTests
+//
+//  Created by UCHIDA Yusuke on 2021/03/06.
+//  Copyright © 2021 tddbc. All rights reserved.
+//
+
+// お題1
+// いちごを出荷するには、品種とサイズを知る必要があります。
+//品種とサイズを与えて、いちごを作成し、その文字列表現を取得しましょう。
+//文字列表現の例: あまおう: S とちおとめ: M もういっこ: L
+//なお、品種は、 あまおう とちおとめ もういっこ の3種類、いちごのサイズは S M L LL の4種類です。
+
+// TODO
+// - [] Strawberryをstructで作成する
+// - [] 品種とサイズを引数に取るコンストラクタを作成する
+// - [] 品種をenum: Stringで定義する
+// - [] サイズをenum: Stringで定義する
+// - [] 文字列表現を取得できる方法を作成する
+// - [] 取得できる文字列表現の書式を決定する
+
+import XCTest
+
+class StrawberryTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
